0a6fcfe3139dac041995062c4db918dfc5c30b81
Veta Sheina <108290460+sssheina@users.noreply.github.com>
2022-12-08 10:45:42 +0300

    Add files via upload

--- assets/scripts/prague.js
+++ assets/scripts/prague.js
@@ -87,3 +87,14 @@ $(function() {
     }
   });
 });
+
+$(function(){
+  // bind change event to select
+  $('#language').on('change', function () {
+      var url = $(this).val(); // get selected value
+      if (url) { // require a URL
+          window.location = url; // redirect
+      }
+      return false;
+  });
+});
